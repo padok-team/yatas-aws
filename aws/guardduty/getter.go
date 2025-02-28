1020717d2392c52d537d07640e80445c6a57043c
Térence Chateigné <terence.chateigne@theodo.com>
2025-02-28 16:39:47 +0100

    feat(guardduty): add check for high severity findings

--- aws/guardduty/getter.go
+++ aws/guardduty/getter.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/guardduty"
+	"github.com/aws/aws-sdk-go-v2/service/guardduty/types"
 	"github.com/padok-team/yatas-aws/logger"
 )
 
@@ -18,4 +19,39 @@ func GetDetectors(s aws.Config) []string {
 		return []string{}
 	}
 	return result.DetectorIds
+}
+
+func GetHighFindings(s aws.Config) []string {
+	svc := guardduty.NewFromConfig(s)
+
+	detectors := GetDetectors(s)
+
+	input := &guardduty.ListFindingsInput{
+		FindingCriteria: &types.FindingCriteria{
+			Criterion: map[string]types.Condition{
+				"severity": {
+					GreaterThanOrEqual: aws.Int64(7), // https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_findings-severity.html#guardduty-finding-severity-level-high
+				},
+				"service.archived": {
+					Equals: []string{"false"},
+				},
+			},
+		},
+	}
+
+	var findings []string
+
+	for _, detector := range detectors {
+		input.DetectorId = &detector
+		result, err := svc.ListFindings(context.TODO(), input)
+		if err != nil {
+			logger.Logger.Error(err.Error())
+			// Return an empty list
+			return []string{}
+		}
+
+		findings = append(findings, result.FindingIds...)
+	}
+
+	return findings
 }
